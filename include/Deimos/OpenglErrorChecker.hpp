13214417cd580444b39553b2ee04ff0f633b3e47
Francisco Neto <cronosts@gmail.com>
2014-11-12 00:01:53 -0300

    cmakefiles now is working very well. Fixed some bugs on deimos code.

--- include/Deimos/OpenglErrorChecker.hpp
+++ include/Deimos/OpenglErrorChecker.hpp
@@ -1,11 +1,11 @@
 #ifndef DEIMOS_OPENGL_ERROR_CHECKER_H_INCLUDED
 #define DEIMOS_OPENGL_ERROR_CHECKER_H_INCLUDED
 
-#ifdef PH_DEBUG
-
 #include <Phobos/Log.h>
 #include <GL/glew.h>
 #include <sstream>
+
+#ifdef PH_DEBUG
 
 #define DEIMOS_GL_CHECK(x)                                          \
     do{                                                             \
